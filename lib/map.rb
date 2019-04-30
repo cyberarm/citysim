aa284aff061d6827d4c3be0ee45d95df3c394738
Cyberarm <matthewlikesrobots@gmail.com>
2019-04-30 18:51:30 -0500

    Added scroll speed, fixed Map#neighbors crashing on out of bounds.

--- lib/map.rb
+++ lib/map.rb
@@ -10,6 +10,7 @@ module CitySim
 
       @money = 30_000
       @citizens = []
+      @scroll_speed = 200
 
       @tool = nil
       @grid = {}
@@ -86,14 +87,18 @@ module CitySim
       else
 
         direction = CyberarmEngine::Vector.new
-        direction.x = -1 if Gosu.button_down?(Gosu::KbD)
         direction.x =  1 if Gosu.button_down?(Gosu::KbA)
+        direction.x = -1 if Gosu.button_down?(Gosu::KbD)
+
         direction.y =  1 if Gosu.button_down?(Gosu::KbW)
         direction.y = -1 if Gosu.button_down?(Gosu::KbS)
 
-        offset = @offset + (direction * @tile_size)
-        offset.x = normalize(offset.x) * @tile_size
-        offset.y = normalize(offset.y) * @tile_size
+        scroll_speed = @scroll_speed
+        if Gosu.button_down?(Gosu::KbLeftControl) || Gosu.button_down?(Gosu::KbRightControl)
+          scroll_speed+= (@scroll_speed*2)
+        end
+
+        offset = @offset + direction.normalized * ((scroll_speed) * window.dt)
         @offset = offset
       end
 
@@ -225,22 +230,31 @@ module CitySim
       position = element.position
       if tool.rows * tool.columns == 1
         # search immediate neighbors only
-        list[:left]  = @grid[position.x - 1][position.y] if @grid[position.x - 1][position.y].element.is_a?(limit)
-        list[:right] = @grid[position.x + 1][position.y] if @grid[position.x + 1][position.y].element.is_a?(limit)
-        list[:up]    = @grid[position.x][position.y - 1] if @grid[position.x][position.y - 1].element.is_a?(limit)
-        list[:down]  = @grid[position.x][position.y + 1] if @grid[position.x][position.y + 1].element.is_a?(limit)
+        list[:left]  = search(position.x - 1, position.y, limit) if search(position.x - 1, position.y, limit)
+        list[:right] = search(position.x + 1, position.y, limit) if search(position.x + 1, position.y, limit)
+        list[:up]    = search(position.x, position.y - 1, limit) if search(position.x, position.y - 1, limit)
+        list[:down]  = search(position.x, position.y + 1, limit) if search(position.x, position.y + 1, limit)
 
         if search == :eight_way
-          list[:top_left]     = @grid[position.x - 1][position.y - 1] if @grid[position.x - 1][position.y - 1].element.is_a?(limit)
-          list[:top_right]    = @grid[position.x + 1][position.y - 1] if @grid[position.x + 1][position.y - 1].element.is_a?(limit)
-          list[:bottom_left]  = @grid[position.x - 1][position.y + 1] if @grid[position.x - 1][position.y + 1].element.is_a?(limit)
-          list[:bottom_right] = @grid[position.x + 1][position.y + 1] if @grid[position.x + 1][position.y + 1].element.is_a?(limit)
+          list[:top_left]     = search(position.x - 1, position.y - 1, limit) if search(position.x - 1, position.y - 1, limit)
+          list[:top_right]    = search(position.x + 1, position.y - 1, limit) if search(position.x + 1, position.y - 1, limit)
+          list[:bottom_left]  = search(position.x - 1, position.y + 1, limit) if search(position.x - 1, position.y + 1, limit)
+          list[:bottom_right] = search(position.x + 1, position.y + 1, limit) if search(position.x + 1, position.y + 1, limit)
         end
       else
         # search edges and return an Array for each side
       end
 
       return list
+    end
+
+    def search(x, y, limit)
+      tile = @grid.dig(x, y)
+      if tile && tile.element.is_a?(limit)
+        tile
+      else
+        nil
+      end
     end
   end
 end
